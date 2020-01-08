b5e762294c98883fe9b2f1a5e6c8f1389641bfa0
Keimeno <constantin@metzworld.com>
2020-01-08 16:36:33 +0100

    refactored some functions and added possibility to reset options

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import Util from './util';
+
 // types import
 import NeojaxOptions from '../types/options';
 import NeojaxHeaders from '../types/headers';
-import NeojaxResponse, { NeojaxConvolutedResponse } from '../types/response.js';
+import NeojaxResponse from '../types/response.js';
 
 // import fetch api
 import 'unfetch/polyfill';
@@ -10,7 +12,7 @@ import 'unfetch/polyfill';
  * Neojax class
  */
 class Neojax {
-	private readonly _options: NeojaxOptions = {};
+	private _options: NeojaxOptions = {};
 	private readonly _defaultHeaders: NeojaxHeaders = {
 		'Content-Type': 'application/json; charset=utf-8'
 	};
@@ -45,80 +47,6 @@ class Neojax {
 	}
 
 	/**
-	 * This is the function, that sends out every request.
-	 *
-	 * NeojaxOptions priority:
-	 *  1. NeojaxOptions specified in every single request
-	 *  2. NeojaxOptions set when creating a new instance through the create method
-	 *  3. Defaultoptions specified in class
-	 *
-	 * @param url
-	 * @param method
-	 * @param data
-	 * @param options
-	 */
-	private async sendRequest(
-		url: string,
-		method: string,
-		data: object | null,
-		options?: NeojaxOptions
-	): Promise<NeojaxConvolutedResponse> {
-		let finalUrl = url;
-		let headers: NeojaxHeaders = this._options.headers || {};
-
-		if (this._options.baseUrl) {
-			finalUrl = this._options.baseUrl + url;
-		}
-
-		if (options) {
-			if (options.baseUrl) {
-				finalUrl = options.baseUrl + url;
-			}
-
-			if (options.headers) {
-				headers = {
-					...headers,
-					...options.headers
-				};
-			}
-		}
-
-		const init: RequestInit = {
-			method,
-			headers: {
-				...headers
-			}
-		};
-
-		if (data) {
-			init.body = JSON.stringify(data);
-		}
-
-		const response = await fetch(finalUrl, init);
-		let body: object | string | number | boolean;
-
-		headers = this.parseHeadersToNeojaxHeaders(response.headers);
-		try {
-			body = await response.json();
-		} catch (e) {
-			try {
-				body = await response.text();
-			} catch (err) {
-				body = {};
-			}
-		}
-
-		return {
-			status: response.status,
-			headers,
-			url: response.url,
-			success: response.ok,
-			message: response.statusText,
-			data: body
-		};
-	}
-
-	/**
 	 * Manages all incoming requests,
 	 * returns neojaxerror or neojaxresponse as promise
 	 *
@@ -134,7 +62,7 @@ class Neojax {
 		options?: NeojaxOptions
 	): Promise<NeojaxResponse> {
 		return new Promise<NeojaxResponse>(async (resolve, reject) => {
-			const response = await this.sendRequest(url, method, data, options);
+			const response = await Util.sendRequest(url, method, data, this._options, options);
 
 			if (response.success) {
 				resolve(response);
@@ -145,21 +73,6 @@ class Neojax {
 				});
 			}
 		});
-	}
-
-	/**
-	 * Parses fetch headers into useable NeojaxHeaders
-	 *
-	 * @param headers
-	 */
-	private parseHeadersToNeojaxHeaders(headers: Headers): NeojaxHeaders {
-		const newHeaders: NeojaxHeaders = {};
-
-		headers.forEach((value: string, name: string) => {
-			newHeaders[name] = value;
-		});
-
-		return newHeaders;
 	}
 
 	/**
@@ -222,6 +135,13 @@ class Neojax {
 	 */
 	public get options(): NeojaxOptions {
 		return this._options;
+	}
+
+	/**
+	 * makes it possible to change options
+	 */
+	public set options(options: NeojaxOptions) {
+		this._options = options;
 	}
 
 	/**
