48991829392a75992c8e44cf77d13c558954431c
Keimeno <constantin@metzworld.com>
2020-01-08 15:30:29 +0100

    changed push command

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
 
 setup_git() {
+  rev=$(git rev-parse --short HEAD)
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
 }
 
 commit_website_files() {
   npm run build
-  git status
-  git add dist
-  git status
+  git add *
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
   git remote add origin-neojax https://${GH_TOKEN}@github.com/Keimeno/neojax.git
-  git push origin-neojax develop
+  git push origin-neojax HEAD:develop
 }
 
 setup_git
