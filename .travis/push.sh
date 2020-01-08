db99e28d3722cd310c0a2525efb964c8a0bedaed
Keimeno <constantin@metzworld.com>
2020-01-08 13:28:31 +0100

    added travis push bash and codecov

---
+++ .travis/push.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git checkout -b neojax-build
+  git add dist
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin-neojax https://${GH_TOKEN}@github.com/Keimeno/neojax.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-neojax neojax-build
+}
+
+setup_git
+commit_website_files
+upload_files
